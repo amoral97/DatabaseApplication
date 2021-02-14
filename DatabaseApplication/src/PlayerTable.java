e1ff1fe036033ecb4772b12f0ea8c1414e46cba4
amoral97 <70278391+amoral97@users.noreply.github.com>
2021-02-14 00:26:57 -0700

    Created GUI With TableView Element For The SqlPlayer Class
    Added General JavaFX Files, just need to configure vm launch args.
    
    Created the TableView Controller which handles most of the GUI
    processing.
    
    Created DBConnector class to separate getting the connection string.
    
    Created PlayerTable class with necessary constructors, getters & setters
    for the TableView and TableColumns.
    
    Created Table.fxml that handles GUI Elements; can open with Scene
    Builder for easy editing of GUI.
    
    NOTE: If program doesn't run please double check the Run Configuration
    VM arguments are correct or javafx libraries are working correctly. If
    not I refer to this video:
    https://www.youtube.com/watch?v=bC4XB6JAaoU&t=323s
    
    If SQLExceptions occur you can mess around with the statement executions
    in the TableViewController initialize method.

---
+++ DatabaseApplication/src/PlayerTable.java
@@ -0,0 +1,74 @@
+/**
+ * NOTE: The TableView JavaFx elements automatically allow for sortation of
+ * desired column (e.g. sort by id by clicking the arrow on the column header in
+ * the UI).
+ * 
+ * Return types must be String in order for the columns to update correctly, if
+ * processing is needed it must be done in the moment by parsing then returned
+ * back to a string value (I could be wrong though :) ).
+ *
+ */
+public class PlayerTable {
+
+	String playerID, firstName, lastName, position, jersey, TeamID;
+
+	public PlayerTable(String playerID, String firstName, String lastName, String position, String jersey,
+			String teamID) {
+		super();
+		this.playerID = playerID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.position = position;
+		this.jersey = jersey;
+		TeamID = teamID;
+	}
+
+	public String getPlayerID() {
+		return playerID;
+	}
+
+	public void setPlayerID(String playerID) {
+		this.playerID = playerID;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public String getJersey() {
+		return jersey;
+	}
+
+	public void setJersey(String jersey) {
+		this.jersey = jersey;
+	}
+
+	public String getTeamID() {
+		return TeamID;
+	}
+
+	public void setTeamID(String teamID) {
+		TeamID = teamID;
+	}
+
+}
