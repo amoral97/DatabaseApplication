28a91a3b11ee95c9291079638accd43d5aadc59f
amoral97 <70278391+amoral97@users.noreply.github.com>
2021-02-14 00:39:22 -0700

    Created GUI with TableView elements for the SqlPlayer Class data
    Updated the DatabaseMain class.
    
    Created Table.fxml with the general GUI elements; can be opened with Scene Builder for easy GUI editing.
    
    Created the TableController class which handles most of the Table processing.
    
    Created the PlayerTable Class which has the constructor, setters and getters for retrieving and updating data from the SqlPlayer class.
    
    Created the DBConnector class to store the SQL connection string.
    
    NOTE: If the program doesn't run correctly make sure that you have the run configuration vm argument configured and that the appropriate javaFX build paths are installed. If not I refer to this video: https://www.youtube.com/watch?v=bC4XB6JAaoU&t=323s
    
    Also, if any SQLExceptions occur, mess around with the execution statements in the TableController class.

--- DatabaseApplication/src/DatabaseMain.java
+++ DatabaseApplication/src/DatabaseMain.java
@@ -1,55 +1,65 @@
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+public class DatabaseMain {
 
-public class DatabaseMain extends Application {
-
+	private static String databaseURL = "jdbc:derby:DatabaseApplication;create=true";
+	
 	public static void main(String[] args) {
+		try (Connection connection = DriverManager.getConnection(databaseURL);
+			Statement statement = connection.createStatement()) {
 
-		launch(args);
+			
+			statement.execute(SqlPlayer.dropTable());
+			statement.execute(SqlPlayer.createTable());
+			statement.execute(SqlPlayer.insertData());
+			ResultSet playerResults = statement.executeQuery(SqlPlayer.allData());
+			printPlayerData(playerResults);
+
+			statement.execute(SqlTeam.dropTable());
+			statement.execute(SqlTeam.createTable());
+			statement.execute(SqlTeam.insertData());
+			ResultSet teamResults = statement.executeQuery(SqlTeam.allData());
+			printTeamData(teamResults);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
-	/*
-	 * private static void printPlayerData(ResultSet results) throws SQLException {
-	 * System.out.println("Player Data:");
-	 * System.out.println("ID  FirstName LastName Position      Jersey TeamID");
-	 * System.out.println("---------------------------------------------------");
-	 * 
-	 * while (results.next()) { int id = results.getInt("ID"); String firstName =
-	 * results.getString("FirstName"); String lastName =
-	 * results.getString("LastName"); String position =
-	 * results.getString("Position"); int jerseyNumber =
-	 * results.getInt("JerseyNumber"); int teamID = results.getInt("TeamID");
-	 * System.out.printf("%3d %-9s %-8s %-13s %-7d %d%n", id, firstName, lastName,
-	 * position, jerseyNumber, teamID); } System.out.println(); }
-	 * 
-	 * private static void printTeamData(ResultSet results) throws SQLException {
-	 * System.out.println("Team Data:");
-	 * System.out.println("ID Name     City      Mascot");
-	 * System.out.println("------------------------------");
-	 * 
-	 * while (results.next()) { int id = results.getInt("ID"); String name =
-	 * results.getString("Name"); String city = results.getString("City"); String
-	 * mascot = results.getString("Mascot");
-	 * System.out.printf("%-2d %-8s %-9s %-7s %n", id, name, city, mascot); }
-	 * System.out.println(); }
-	 */
-	@Override
-	public void start(Stage primaryStage) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("Table.fxml"));
-		Scene sc = new Scene(root);
+	private static void printPlayerData(ResultSet results) throws SQLException {
+		System.out.println("Player Data:");
+		System.out.println("ID  FirstName LastName Position      Jersey TeamID");
+		System.out.println("---------------------------------------------------");
 
-		primaryStage.setScene(sc);
-		primaryStage.show();
+		while (results.next()) {
+			int id = results.getInt("ID");
+			String firstName = results.getString("FirstName");
+			String lastName = results.getString("LastName");
+			String position = results.getString("Position");
+			int jerseyNumber = results.getInt("JerseyNumber");
+			int teamID = results.getInt("TeamID");
+			System.out.printf("%3d %-9s %-8s %-13s %-7d %d%n", id, firstName, lastName, position, jerseyNumber, teamID);
+		}
+		System.out.println();
+	}
+	
+	private static void printTeamData(ResultSet results) throws SQLException {
+		System.out.println("Team Data:");
+		System.out.println("ID Name     City      Mascot");
+		System.out.println("------------------------------");
+
+		while (results.next()) {
+			int id = results.getInt("ID");
+			String name = results.getString("Name");
+			String city = results.getString("City");
+			String mascot = results.getString("Mascot");
+			System.out.printf("%-2d %-8s %-9s %-7s %n", id, name, city, mascot);
+		}
+		System.out.println();
 	}
 }
